0e43d876ad188015eb46e3347725af3c21430c35
applejuicehasini2 <srijanidhi09@gmail.com>
2025-06-09 17:19:45 +0200

    Add files via upload

---
+++ greatest ot smallest.py
@@ -0,0 +1,8 @@
+i=int(input("enter a number:"))
+if ( 1 < 15):
+    print("i is smaller than 15")
+    print("i'm in if block ")
+else:
+    print("i is greater than 15 ")
+    print("i'm in else block ")
+print("i'm not in if or not else in else block")

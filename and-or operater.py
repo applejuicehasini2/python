1497cf3449450074e80ebbd77c8c381520876b6f
applejuicehasini2 <srijanidhi09@gmail.com>
2025-06-23 17:15:18 +0200

    Add files via upload

---
+++ and-or operater.py
@@ -0,0 +1,19 @@
+a=10
+b=12
+c=0
+if 1 and b and c:
+    print("all variables are non-zero")
+else:
+    print("at least on variables are zero")
+a=10
+b=10
+c=0
+if a > 0 or b > 0:
+    print("print aleast one varible is poisitive")
+else:
+    print("both variables are non positive")
+if b > 0 and c > 0:
+    print("either of number is greater than zero")
+else:
+    print("both numbers are less than or equal to 0 ")
+    

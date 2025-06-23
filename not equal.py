fb0fbf30c245839dbddf58f7c2e447eee7e73cbf
applejuicehasini2 <srijanidhi09@gmail.com>
2025-06-23 17:15:48 +0200

    Add files via upload

---
+++ not equal.py
@@ -0,0 +1,16 @@
+a=10
+b=12
+c=12
+print(a !=b)
+print(b != c)
+a= "python"
+b= "coding"
+if a !=b:
+    print("a b are not equal")
+a=4
+b=5
+if (a == 1) != (b ==5 ):
+    print("hello")
+a=int(input("enter a number: "))
+if a%2 != 0:
+    print(a, "its not an equal number")

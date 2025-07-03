0a2c8dc9bd1765a403133bbb9a577d00144770f5
applejuicehasini2 <srijanidhi09@gmail.com>
2025-07-03 17:22:03 +0200

    Add files via upload

---
+++ operater.py
@@ -0,0 +1,13 @@
+v=4
+w=5
+x=8
+y=2
+z=0
+z=(v+w)*x/y;
+print("value of (v+w)*x/y) is",z)
+name = "Alex"
+age = 0
+if name == "Alex" or name == "john" and age>= 2:
+    print("name is Alex or John and age is greater or equal to 2")
+else:
+    print("name is not Alex or John or age younger is younger than 2")

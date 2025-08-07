b046bddbc6c37f0c53ed8a591cc94a3d036300d1
applejuicehasini2 <srijanidhi09@gmail.com>
2025-08-07 17:23:10 +0200

    Add files via upload

---
+++ weather_condition.py
@@ -0,0 +1,6 @@
+def weather_condition():
+    print(' the weather is  pleasent  ', spring)
+    print(' the weather is too pleasent  ', autumn)
+spring = "autumn"
+autumn = spring
+weather_condition()
